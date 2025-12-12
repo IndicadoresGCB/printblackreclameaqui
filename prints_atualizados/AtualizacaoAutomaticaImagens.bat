288c5442a0af6851bdb8c4e6ca35a42b3d0fbbf8
CarlosAFilhoEngDados <carlos.fil.cec@gmail.com>
2025-12-12 08:39:55 -0300

    Atualizacao de imagens via script .bat

--- prints_atualizados/AtualizacaoAutomaticaImagens.bat
+++ prints_atualizados/AtualizacaoAutomaticaImagens.bat
@@ -4,24 +4,6 @@ echo  Atualizando Imagens no GitHub
 echo ================================
 echo Created by : Carlos Filho
 echo.
-
-REM Caminho local da pasta onde estão as imagens
-SET PASTA=C:\Users\2160040883\OneDrive - Grupo Casas Bahia S.A\Blackfriday-RA\printblackreclameaqui\prints_atualizados
-
-echo Verificando se as imagens existem...
-if not exist "%PASTA%\Ranking_geral.png" (
-    echo ERRO: Ranking_geral.png não encontrada!
-) else (
-    echo OK: Ranking_geral.png encontrada.
-)
-
-if not exist "%PASTA%\Ranking_varejo.png" (
-    echo ERRO: Ranking_varejo.png não encontrada!
-) else (
-    echo OK: Ranking_varejo.png encontrada.
-)
-
-echo.
 echo Fazendo commit e push...
 git add .
 git commit -m "Atualizacao de imagens via script .bat"
